e1327802ba6e86354bc30adb04eb094c265647bc
John Olawale <jolawale@softalliance.com>
2017-05-07 04:28:12 +0100

    reset

--- application/views/pages/index.php
+++
@@ -1,9 +0,0 @@
-<h1>Hello!, Welcome to my application</h1>
-
-<?php if ($this->ion_auth->logged_in()) : ?>
-    <h4>You are Logged in</h4>
-<?php else: ?>
-    <h4>You are not logged in</h4>
-<?php endif; ?>
-
-<?php print_r( $user ); ?>
